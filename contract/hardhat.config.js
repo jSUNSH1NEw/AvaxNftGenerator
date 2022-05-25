b01c28e34d13fc60c563d081841dd92d5a71c67f
jSUNSH1NEw <weertz.joffrey@protonmail.ch>
2022-05-25 16:39:27 +0200

    init

---
+++ contract/hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomiclabs/hardhat-waffle");
+
+// Replace these private key with your Avalanche account private keys
+// Be aware of NEVER adding private keys to GIT
+const AVALANCHE_TEST_PRIVATE_KEY = "PRIVATE_KEY_FOR_FIJI";
+const AVALANCHE_MAIN_PRIVATE_KEY = "PRIVATE_KEY_FOR_MAINNET";
+
+module.exports = {
+  solidity: "0.7.3",
+};
